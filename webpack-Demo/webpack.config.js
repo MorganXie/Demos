77d4236a5eda35cc296cab6e27afed180271e881
MorganXie <529303860@qq.com>
2017-09-14 20:39:18 +0800

    path

--- webpack-Demo/webpack.config.js
+++ webpack-Demo/webpack.config.js
@@ -1,10 +1,11 @@
 var webpack = require('webpack');
+var path  = require ('path');
 module.exports = {
 	//定义打包入口文件
 	entry:'./js/index.js',
 	//定义打包输出文件路径
 	output:{
-		path:'/Users/xx/Desktop/webpack-Demo/bin',
+		path:path.resolve(__dirname, 'bin/'),
 		filename:'webpack.merge.js'
 	}
 	
